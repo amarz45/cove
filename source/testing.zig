31589aa86c7cc38b8818b6f1a995b34ed23c9061
Amar Al-Zubaidi <amar.al-zubaidi45@tutanota.com>
2025-02-21 21:35:16 -0500

    Add testing file

---
+++ source/testing.zig
@@ -0,0 +1,9 @@
+const std = @import("std");
+
+pub inline fn expect_eql(actual: anytype, expected: @TypeOf(actual)) !void {
+    return std.testing.expectEqual(expected, actual);
+}
+
+pub inline fn expect_eql_str(actual: []const u8, expected: []const u8) !void {
+    return std.testing.expectEqualStrings(expected, actual);
+}
